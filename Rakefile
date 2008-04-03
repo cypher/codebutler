bc6c180c452395b235d3475190592f40c5188533
Markus Prinz <markus.prinz@qsig.org>
2008-04-03 21:53:02 +0200

    Initial checkin

---
+++ Rakefile
@@ -0,0 +1,14 @@
+require 'rubygems'
+require 'rake'
+require 'echoe'
+
+Echoe.new("codebutler") do |p|
+  p.author = "Markus Prinz"
+  p.email = "markus.prinz@qsig.org"
+  p.summary = "Easily serve your hilighted codefiles from a local webserver"
+  p.description = "Simply run codebutler in a directory of your choice, and it will serve all the code files it supports on a local webserver, hilighted."
+  p.url = "http://codebutler.rubyforge.org"
+  p.version = '0.0.1'
+  p.dependencies = ["sinatra >=0.1.7", "coderay >=0.7.4"]
+end
+
---
+++ lib/codebutler.rb
@@ -0,0 +1,47 @@
+$:.unshift(File.dirname(__FILE__)) unless $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+
+require 'find'
+require 'coderay'
+require 'sinatra'
+
+module CodeButler
+  
+  SUPPORTED_LANGUAGES = {
+    '.rb'     => :ruby,
+    '.c'      => :c,
+    '.cc'     => :c,
+    '.h'      => :c,
+    '.html'   => :html,
+    '.rhtml'  => :rhtml,    
+  }
+  
+  # Scan the current working directory for files, and serve them on localhost
+  def self.serve
+    # Create a regex that matches all supported file endings
+    regex = Regexp.new("(#{SUPPORTED_LANGUAGES.keys.map{|key| Regexp.escape(key)}.join('|')})$")
+    
+    working_dir = Dir.getwd
+    file_list = []
+
+    Find.find(working_dir) do |path|
+      if match = path.match(regex)
+        relative_path = path.slice( working_dir.length..-1 )
+        
+        file_list << relative_path
+        
+        # use Sinatra for easy hosting
+        get(relative_path) do
+          CodeRay.scan(File.readlines(path).join, SUPPORTED_LANGUAGES[match[1]]).div
+        end
+      end
+    end
+    
+    # index:
+    get('/') do
+      file_list.sort.map { |file|
+        %Q(<a href="#{file}">#{file}</a>)
+      }.join("<br/>")      
+    end
+    
+  end
+end
